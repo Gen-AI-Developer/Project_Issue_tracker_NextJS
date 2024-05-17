3b8f3c6f78652deb8b41e721829d45a6df55300d
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-17 11:07:54 +0500

    NavBarCompleted

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import { VscDebugConsole } from "react-icons/vsc";
 const NavBar = () => {
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ]
     return (
         <nav className='flex space-x-12 border-b mb-5 px-5 h-14 items-center'>
             <Link href={'/'}><VscDebugConsole size={26} /></Link>
             <ul className='flex space-x-4'>
-                <li><Link href={'/'}>Dashboard</Link></li>
-                <li><Link href={'/issues'}>Issues</Link></li>
+                {links.map(link => <Link
+                    key={link.href}
+                    className='text-zinc-400 hover:text-zinc-900 transition-colors'
+                    href={link.href}>{link.label}</Link>)}
+
             </ul>
         </nav>
     )

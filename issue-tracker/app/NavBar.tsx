dcc151bb04689d258d8d596b7384ea71da7bf777
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-19 23:16:20 +0500

    Updates

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -5,7 +5,7 @@ import { MdFitbit } from "react-icons/md";
 import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
 import Image from 'next/image';
-import logo from '../public/vercel.svg'
+import logo from '../app/public/logo.svg'
 
 const NavBar = () => {
     const currentPath = usePathname();
@@ -17,7 +17,7 @@ const NavBar = () => {
     return (
         <nav className='flex bg-white m-3 drop-shadow-lx space-x-12 border rounded-md mb-5 px-5 h-14 items-center'>
 
-            <Link className='flex font-bold' href={'/'}> <Image src={logo} width={35} height={35} alt={''}></Image> DEVSQUAD </Link>
+            <Link className='flex font-bold' href={'/'}> <Image src={logo} width={25} height={25} alt={''}></Image> DevSquads</Link>
             {/* <MdFitbit size={24} color='#242659' /> */}
             <ul className='flex space-x-4 font-semibold'>
                 {links.map(link => <Link
--- issue-tracker/app/page.tsx
+++ issue-tracker/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image'
+import logo from '../app/public/logo.svg'
 export default function Home() {
   return (
     <div className="flex flex-col m-3 p-4 border rounded-lg shadow-md bg-white items-center">
-      <h1 className="text-lg font-bold">Issue Tracking Application</h1>
+
+      <Image src={logo} alt='' width={150} height={150}></Image>
+      <h1 className="text-xl font-bold">Developers Squad</h1>
       <p className="text-slate-700 dark:text-slate-500">Efficiently manage your team's tasks and projects with our cutting-edge
         issue tracking app. Designed for seamless collaboration, our platform
         simplifies the process of reporting, tracking, and resolving issues.

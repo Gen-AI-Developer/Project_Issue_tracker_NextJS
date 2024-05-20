0c5aa0867d7c4a8022e2d8ad1728ca2e2b22ae19
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-20 07:52:24 +0500

    SetUp Radiux UI

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const IssuesPage = () => {
     return (
         <div className="flex flex-col m-3 p-4 border rounded-md shadow-md bg-white items-center">
             <div className='grid-col-1 text-xl font-bold'>ONE</div>
+            <Button>New Issue</Button>
             <div className='grid-col-2 text-xl font-bold'>TWO</div>
         </div>
     )
--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} `}>
-        <NavBar />
-        {children}
+        <Theme>
+          <NavBar />
+          {children}
+        </Theme>
 
       </body>
     </html>

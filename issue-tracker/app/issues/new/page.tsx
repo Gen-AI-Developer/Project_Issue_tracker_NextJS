17f871b64d883cca5095428f86a27686e7a35135
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-21 22:50:30 +0500

    UpdatedIssuePage

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 import { InputIcon } from '@radix-ui/react-icons'
-import { TextArea, TextField, Button, Heading } from '@radix-ui/themes'
+import { TextArea, Text, TextField, Button, Heading, Box, Inset, Card, Strong } from '@radix-ui/themes'
 import Image from 'next/image'
 import React from 'react'
 import logo from '@/app/public/IssuePage.svg'
@@ -34,7 +34,29 @@ const NewIssue = () => {
             </div>
             <div className='ml-4 items-center'>
                 <div>
-                    <Image src={logo} height={350} width={350} alt='logo'></Image>
+                    <Box maxWidth="540px">
+                        <Card size="3">
+                            <Inset clip="padding-box" side="top" pb="current">
+                                <img
+                                    src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    alt="Bold typography"
+                                    style={{
+                                        display: 'block',
+                                        objectFit: 'cover',
+                                        width: '100%',
+                                        height: 140,
+                                        backgroundColor: 'var(--gray-5)',
+                                    }}
+                                />
+                            </Inset>
+
+                            <Text>
+                                <Strong>- DevSquads -</Strong>
+                                Welcome to DevSquads, the ultimate platform where professional developers systematically resolve your coding issues, ensuring that you never encounter the same errors again. Leveraging the power of Generative AI, our team offers precise and efficient solutions tailored to your specific problems. Whether you're dealing with a minor bug or a critical system failure, DevSquads is here to help you debug, optimize, and enhance your code, making your development process smoother and more productive.
+                            </Text>
+                        </Card>
+                    </Box>
+                    {/* <Image src={logo} height={350} width={350} alt='logo'></Image> */}
                 </div>
                 {/* <Heading>Keep it Simple, so our aliens can Understand</Heading> */}
             </div>

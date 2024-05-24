8824d79578e6673cce25db86dfc6a45860880303
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-05-24 19:13:16 +0500

    updated

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -18,48 +18,8 @@ const NewIssue = () => {
     // console.log(register('title'))
     return (
 
-        <div className='m-4 item flex-col  grid grid-cols-1 md:grid-cols-2'>
-            <Form.Root>
-                <Form.Field className="grid mb-[10px]" name="email">
-                    <div className="flex items-baseline justify-between">
-                        <Form.Label className="text-[15px] font-medium leading-[35px]  text-zinc-600">Email</Form.Label>
-                        <Form.Message className="text-[13px] text-zinc-600 opacity-[0.8]" match="valueMissing">
-                            Please enter your email
-                        </Form.Message>
-                        <Form.Message className="text-[13px]  text-zinc-600 opacity-[0.8]" match="typeMismatch">
-                            Please provide a valid email
-                        </Form.Message>
-                    </div>
-                    <Form.Control asChild>
-                        <input
-                            className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6"
-                            type="email"
-                            required
-                        />
-                    </Form.Control>
-                </Form.Field>
-                <Form.Field className="grid mb-[10px]" name="question">
-                    <div className="flex items-baseline justify-between">
-                        <Form.Label className="text-[15px] font-medium leading-[35px]  text-zinc-600">
-                            Question
-                        </Form.Label>
-                        <Form.Message className="text-[13px]  text-zinc-600 opacity-[0.8]" match="valueMissing">
-                            Please enter a question
-                        </Form.Message>
-                    </div>
-                    <Form.Control asChild>
-                        <textarea
-                            className="box-border w-full bg-blackA2 shadow-blackA6 inline-flex appearance-none items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA6 resize-none"
-                            required
-                        />
-                    </Form.Control>
-                </Form.Field>
-                <Form.Submit asChild>
-                    <button className="box-border w-full text-white hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-indigo-600 px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-                        Post question
-                    </button>
-                </Form.Submit>
-            </Form.Root>
+        <div className='m-4 p-3 item flex-col rounded border grid grid-cols-1 md:grid-cols-2'>
+            
             <div className='ml-4'>
                 <div className='mt-6 md:mt-0'>
                     <Box maxWidth="540px">
